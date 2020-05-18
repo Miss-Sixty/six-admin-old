b47863c453a2f0b4217d8e5d05d765658fe6c80e
zxh <im0k@outlook.com>
2020-05-18 13:39:16 +0800

    完善AppMain，添加动画

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="app-main">AppMain</div>
+  <section class="app-main">
+    <transition name="fade-transform" mode="out-in">
+      <router-view :key="key" />
+    </transition>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "AppMain",
+  computed: {
+    key() {
+      return this.$route.path;
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
 .app-main {
   background-color: beige;
+
+  /* 50= navbar  50  */
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  overflow: hidden;
 }
 </style>
