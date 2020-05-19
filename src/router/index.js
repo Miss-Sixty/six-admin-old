6c0f522de4c5365861f570ac54bbb57e92b52ab6
zxh <im0k@outlook.com>
2020-05-19 17:46:24 +0800

    添加错误页面

--- src/router/index.js
+++ src/router/index.js
@@ -64,6 +64,30 @@ export const asyncRoutes = [
         meta: { title: "图标", icon: "icon", noCache: true }
       }
     ]
+  },
+  {
+    path: "/error",
+    component: Layout,
+    redirect: "noRedirect",
+    name: "ErrorPages",
+    meta: {
+      title: "错误页面",
+      icon: "404"
+    },
+    children: [
+      {
+        path: "401",
+        component: () => import("@/views/error-page/401"),
+        name: "Page401",
+        meta: { title: "401", noCache: true }
+      },
+      {
+        path: "404",
+        component: () => import("@/views/error-page/404"),
+        name: "Page404",
+        meta: { title: "404", noCache: true }
+      }
+    ]
   }
   // {
   //   path: "/error",
---
+++ src/views/error-page/404.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="error-404">
+    <img src="@/assets/imgs/404.svg" alt="404" width="500px" />
+    <p>很抱歉，页面不小心迷路了！</p>
+    <a href="" class="error-404__button">回首页</a>
+  </div>
+</template>
+<style lang="scss" scoped>
+.error-404 {
+  margin: 100px auto;
+  text-align: center;
+  p {
+    font-size: 26px;
+    color: #666;
+  }
+  &__button {
+    display: inline-block;
+    width: 110px;
+    height: 36px;
+    background-color: #1482f0;
+    border-radius: 100px;
+    text-align: center;
+    color: #ffffff;
+
+    font-size: 14px;
+    line-height: 36px;
+    cursor: pointer;
+    animation-name: slideUp;
+    animation-duration: 0.5s;
+    animation-delay: 0.3s;
+    animation-fill-mode: forwards;
+  }
+}
+</style>
