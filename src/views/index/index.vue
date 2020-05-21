966d5f7e7f85cde34c52080c8a5085f7cf128b69
zxh <im0k@outlook.com>
2020-05-21 13:28:25 +0800

    首页添加说明

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>index</div>
+  <div class="app-container">
+    <aside>
+      此项目是作者用来快速构建后台所使用的模板，各项功能在不断完善中，也借鉴了一些其他优秀
+      <a href>开源项目</a>
+      的代码。如有兴趣可 fork 下来自行查看。
+    </aside>
+  </div>
 </template>
 <script>
 export default {};
