d0906e38dfa03511afdb9cedf00fccc9debaa996
zxh <im0k@outlook.com>
2020-05-26 21:15:02 +0800

    添加 导出选中项路由  和 导出多级表头路由

--- src/router/modules/excel.js
+++ src/router/modules/excel.js
@@ -2,7 +2,6 @@ import Layout from "@/layout";
 export default {
   path: "/excel",
   component: Layout,
-  redirect: "/excel/export-excel",
   name: "Excel",
   meta: {
     title: "Excel",
@@ -10,16 +9,28 @@ export default {
   },
   children: [
     {
-      path: "export-excel",
-      component: () => import("@/views/excel/export-excel"),
-      name: "ExportExcel",
-      meta: { title: "导出Excel" }
-    },
-    {
       path: "upload-excel",
       component: () => import("@/views/excel/upload-excel"),
       name: "UploadExcel",
-      meta: { title: "上传Excel" }
+      meta: { title: "上传 Excel" }
+    },
+    {
+      path: "export-excel",
+      component: () => import("@/views/excel/export-excel"),
+      name: "ExportExcel",
+      meta: { title: "导出 Excel" }
+    },
+    {
+      path: "export-selected-excel",
+      component: () => import("@/views/excel/select-excel"),
+      name: "SelectExcel",
+      meta: { title: "导出 选中项" }
+    },
+    {
+      path: "export-merge-header",
+      component: () => import("@/views/excel/merge-header"),
+      name: "MergeHeader",
+      meta: { title: "导出 多级表头" }
     }
   ]
 };
---
+++ src/views/excel/merge-header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="app-container">
+    merge-header
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
